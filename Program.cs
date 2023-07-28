f4b4a90aba8f4cbe991d87ec0f1ecaac83dc6873
satinder <satindersingh472@gmail.com>
2023-07-28 07:17:45 -0600

    finihsed

--- Program.cs
+++ Program.cs
@@ -3,11 +3,22 @@ class Program
 {
     static void Main(string[] args)
     {
-       int principle_amount = 29000;
-       double rate = 6.99;
-       int time_years = 5;
+        double principle_amount = 5000;
+        double rate = 0.08;
+        int time_years = 2;
+        int time_compounded = 4;
 
-    double simple_interest = (principle_amount * rate * time_years)/100;
-    Console.WriteLine($"Simple Interest on an amount of {principle_amount} for {time_years} is --- {simple_interest}");
+        double simple_interest = principle_amount * rate * time_years;
+        Console.WriteLine($"Simple Interest on an amount of {principle_amount} for {time_years} years is --- {simple_interest}");
+
+        double total_quarters = time_compounded * time_years;
+        double interest_every_quarter = (1 + (rate / time_compounded));
+        double powerValue = Math.Pow(interest_every_quarter, total_quarters);
+
+        double amount_after_interest = principle_amount * powerValue;
+        amount_after_interest = (Math.Round(amount_after_interest, 2, MidpointRounding.AwayFromZero));
+        double total_interest = amount_after_interest - principle_amount;
+        Console.WriteLine($"Compound Interest on an amount of {principle_amount} for {time_years} years is --- {total_interest}");
+
     }
 }
