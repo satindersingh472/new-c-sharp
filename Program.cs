c95a7353d8cf60622680584e9547cc0305459c9d
satinder <satindersingh472@gmail.com>
2023-07-27 21:34:07 -0600

    started

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace console_app;
+class Program
+{
+    static void Main(string[] args)
+    {
+       int principle_amount = 29000;
+       double rate = 6.99;
+       int time_years = 5;
+
+    double simple_interest = (principle_amount * rate * time_years)/100;
+    Console.WriteLine($"Simple Interest on an amount of {principle_amount} for {time_years} is --- {simple_interest}");
+    }
+}
